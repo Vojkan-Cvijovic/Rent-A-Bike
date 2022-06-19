74d2bd7eccae83d874285412f0d658f43e8b6742
Vojkan-Cvijovic <vojkan94@ymail.com>
2022-06-19 23:55:13 +0200

    Add update function

---
+++ src/api/functions/bikes/update.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const dynamoDb = require("../../config/dynamoDb");
+const { sendResponse } = require("../../utils/common");
+
+module.exports.updateBike = async event => {
+    try {
+        const body = JSON.parse(event.body);
+        const {id, active, used} = body;
+        const params = {
+            TableName: process.env.BIKES_TABLE_NAME,
+            Key: {
+                id
+            },
+            ExpressionAttributeValues: {
+                ":active": active,
+                ":used": used
+            },
+            UpdateExpression:
+                "SET active = :active, used = :used",
+            ReturnValues: "ALL_NEW"
+        };
+        const data = await dynamoDb.update(params).promise();
+        if (data.Attributes) {
+            return sendResponse(200, data.Attributes);
+        } else {
+            return sendResponse(404, {message: "Could not find bike with id " + id});
+        }
+    } catch (e) {
+        return sendResponse(500, {message: "Internal server error"});
+
+    }
+};
