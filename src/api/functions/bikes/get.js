bca1e2c06a4050246182e160f3251faac8e59ddc
Vojkan-Cvijovic <vojkan94@ymail.com>
2022-06-19 22:57:43 +0200

    Create get function

---
+++ src/api/functions/bikes/get.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const dynamoDb = require("../../config/dynamoDb");
+const { sendResponse } = require("../../utils/common");
+
+module.exports.getBike = async event => {
+    try {
+        const { id } = event.pathParameters;
+        const params = {
+            TableName: process.env.BIKES_TABLE_NAME,
+            KeyConditionExpression: "id = :id",
+            ExpressionAttributeValues: {
+                ":id": id
+            },
+            Select: "ALL_ATTRIBUTES"
+        };
+
+        const data = await dynamoDb.query(params).promise();
+        if (data.Count > 0) {
+            return sendResponse(200, {payload: data.Items});
+        } else {
+            return sendResponse(404, {message: "Bike not found"});
+        }
+    } catch (e) {
+        return sendResponse(500, {message: "Internal error"});
+    }
+};
+
+
