d37f1282a040554ad875b39cd1bab8f09266bc8e
Vojkan-Cvijovic <vojkan94@ymail.com>
2022-06-20 00:02:17 +0200

    Add delete function

---
+++ src/api/functions/bikes/delete.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const dynamoDb = require("../../config/dynamoDb");
+const { sendResponse } = require("../../utils/common");
+
+module.exports.deleteBike = async event => {
+    try {
+        const body = JSON.parse(event.body);
+        const { id } = body;
+        const params = {
+            TableName: process.env.BIKES_TABLE_NAME,
+            Key: {
+                id
+            }
+        };
+        await dynamoDb.delete(params).promise();
+        return sendResponse(200, { message: "Removed bike with id " + id});
+    } catch (e) {
+        return sendResponse(500, {message: "Internal server error"});
+    }
+};
+
