fa0481d062e102d532ee55001be4ece7fb94446c
Vojkan-Cvijovic <vojkan94@ymail.com>
2022-07-11 23:24:09 +0200

    List avaliable bikes per selected location

--- src/api/functions/bikes/listAvailable.js
+++ src/api/functions/bikes/listAvailable.js
@@ -3,15 +3,21 @@
 const dynamoDb = require("../../config/dynamoDb");
 const { sendResponse } = require("../../utils/common");
 
-module.exports.listAvailableBikes = async () => {
+module.exports.listAvailableBikes = async event => {
     try {
+        const { location } = event.pathParameters;
+        const selectedLocation = decodeURI(location);
+        console.log("Fetching bikes for location " + selectedLocation);
         const params = {
             TableName: process.env.BIKES_TABLE_NAME,
-            FilterExpression: 'active = :active and used = :used',
+            // location is reserved keyword, using #dynobase_location instead
+            FilterExpression: 'active = :active and used = :used and #dynobase_location = :location',
             ExpressionAttributeValues: {
                 ':active': true,
-                ':used': false
-            }
+                ':used': false,
+                ':location': selectedLocation
+            },
+            ExpressionAttributeNames: { "#dynobase_location": "location" }
         };
         const bikes = await dynamoDb.scan(params).promise();
         return sendResponse(200, bikes.Items);
